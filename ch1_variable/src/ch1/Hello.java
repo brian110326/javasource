8a296113092d92890902fc4d74fbe09cd2345b4b
brian110326 <brian110326@naver.com>
2024-01-30 17:50:22 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/Hello.java
@@ -0,0 +1,13 @@
+package ch1;
+
+// 클래스 이름은 대문자로 시작
+// 클래스명은 한글이 가능하지만, 영어로 사용
+// shift + alt + 아래화살표 : 복 / 붙
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
